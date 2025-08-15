361f69ca6a6d6999349399ee710e513c3e2d1cd5
cefika <cefika99@live.com>
2025-08-15 22:39:31 +0200

    change from BigInt to parseDecimal

--- src/rpc/evm.ts
+++ src/rpc/evm.ts
@@ -300,7 +300,7 @@ class EvmRpc extends Rpc {
         data: encodeFunctionData({
           abi: erc20Abi,
           functionName: "approve",
-          args: [csucContractAddress, BigInt(amount)],
+          args: [csucContractAddress, parseDecimal(amount,token)],
         }),
         gas: 70_000n,
         nonce,
@@ -311,7 +311,7 @@ class EvmRpc extends Rpc {
         data: encodeFunctionData({
           abi: CSUC_ETH_SEPOLIA_ARTIFACT.abi,
           functionName: "wrapERC20",
-          args: [toAddress, token.contractAddress as `0x${string}`, BigInt(amount)],
+          args: [toAddress, token.contractAddress as `0x${string}`, parseDecimal(amount,token)],
         }),
         gas: 120_000n,
         nonce: ++nonce,
