97ac980617354519e22d7001b2ed7ed055bf86e8
Lazar Travica <lazar.travica@route3.co>
2025-08-22 11:42:27 +0200

    local-curvy.name

--- src/constants/curvy.ts
+++ src/constants/curvy.ts
@@ -1,3 +1,3 @@
-const CURVY_HANDLE_DOMAINS = [".staging-curvy.name", ".curvy.name"] as const;
+const CURVY_HANDLE_DOMAINS = [".staging-curvy.name", ".curvy.name", ".local-curvy.name"] as const;
 
 export { CURVY_HANDLE_DOMAINS };
