558544a6bfe3c99f4ebb99a88bf963f1c32a00ae
Vanja Obradovic <vanja.obradovic99@gmail.com>
2025-08-29 12:31:05 +0200

    refactor: remove redundant files from fe

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -5,14 +5,18 @@ function toSlug(str: string) {
   return str.replace(" ", "-").toLowerCase();
 }
 
-const sha256Digest = async (message: string, _outputLength: number | undefined = undefined): Promise<string> => {
-  const hash = await crypto.subtle.digest("SHA-256", textEncoder.encode(message));
+const shaDigest = async (
+  alg: "SHA-1" | "SHA-256" | "SHA-384" | "SHA-512",
+  message: string,
+  _outputLength: number | undefined = undefined,
+): Promise<string> => {
+  const hash = await crypto.subtle.digest(alg, textEncoder.encode(message));
   return Buffer.from(hash).toString("hex").slice(0, undefined);
 };
 
 const WALLET_ID_LENGTH = 12;
 const generateWalletId = (s: string, v: string) => {
-  return sha256Digest(JSON.stringify({ s, v }), WALLET_ID_LENGTH);
+  return shaDigest("SHA-256", JSON.stringify({ s, v }), WALLET_ID_LENGTH);
 };
 
 function arrayBufferToHex(buffer: ArrayBuffer) {
@@ -25,4 +29,4 @@ function encode(message: string) {
   return textEncoder.encode(message);
 }
 
-export { isNode, sha256Digest, generateWalletId, textEncoder, toSlug, arrayBufferToHex, encode };
+export { isNode, shaDigest, generateWalletId, textEncoder, toSlug, arrayBufferToHex, encode };
---
+++ src/utils/index.ts
@@ -0,0 +1,7 @@
+// BARREL FILE FOR UTILITY EXPORTS
+
+export * from "./common";
+export * from "./csuc";
+export * from "./currency";
+export * from "./encryption";
+export { shaDigest } from "./helpers";
