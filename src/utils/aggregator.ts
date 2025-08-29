a66bf8d01bf6594d34bfce7fd4e5d76ea43f5d2e
Aleksandar Veljkovic <aleksandar.veljkovic.dev@gmail.com>
2025-08-29 12:34:27 +0200

    aggregator utility functions

---
+++ src/utils/aggregator.ts
@@ -0,0 +1,67 @@
+import { poseidon2, poseidon3 } from "poseidon-lite";
+
+const generateDummyOutputNote = (token?: string) => ({
+  ownerHash: BigInt(
+    `0x${Buffer.from(crypto.getRandomValues(new Uint8Array(31))).toString(
+      "hex"
+    )}`
+  ).toString(),
+  token: token || "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",
+  amount: "0",
+  viewTag: Buffer.from(crypto.getRandomValues(new Uint8Array(1)))
+    .toString("hex")
+    .slice(0, 2)
+    .padStart(2, "0"),
+  ephemeralKey: `0x${Buffer.from(
+    crypto.getRandomValues(new Uint8Array(31))
+  ).toString("hex")}`,
+});
+
+const padAggregationOutputNotes = (
+  outputNotes: any,
+  numberOfNotes: number = 2
+) => {
+  if (outputNotes.length === 0) {
+    throw new Error("Missing primary output note");
+  }
+  const paddedOutputNotes = [...outputNotes];
+
+  for (let i = outputNotes.length; i < numberOfNotes; i++) {
+    paddedOutputNotes.push(generateDummyOutputNote());
+  }
+
+  return paddedOutputNotes;
+};
+
+const generateOutputNoteHash = (outputNote: any) => {
+  return poseidon3([
+    BigInt(outputNote.ownerHash),
+    BigInt(outputNote.amount),
+    BigInt(outputNote.token),
+  ]);
+};
+
+const generateAggregationOutputsHash = (outputNotes: any) => {
+  const outputNoteHashes = outputNotes.map(generateOutputNoteHash);
+  return poseidon2(outputNoteHashes);
+};
+
+const generateEphemeralKeysHash = (outputNotes: any[]) => {
+  const ephemeralKeys = outputNotes.map((note: any) => note.ephemeralKey);
+  return poseidon2(ephemeralKeys);
+};
+
+const generateAggregationHash = (outputNotes: any) => {
+  const outputNoteHash = generateAggregationOutputsHash(outputNotes);
+  const ephemeralKeyHash = generateEphemeralKeysHash(outputNotes);
+  return poseidon2([outputNoteHash, ephemeralKeyHash]);
+};
+
+export {
+  generateDummyOutputNote,
+  padAggregationOutputNotes,
+  generateOutputNoteHash,
+  generateAggregationOutputsHash,
+  generateEphemeralKeysHash,
+  generateAggregationHash,
+};
---
+++ tests/aggregator-utils.test.ts
@@ -0,0 +1,80 @@
+import {
+  generateDummyOutputNote,
+  generateEphemeralKeysHash,
+  generateOutputNoteHash,
+  padAggregationOutputNotes,
+  generateAggregationHash,
+} from "@/utils/aggregator";
+import { expect, test } from "vitest";
+import { poseidon2, poseidon3 } from "poseidon-lite";
+
+test("should generate dummy aggregation output note", async () => {
+  const outputNote = generateDummyOutputNote();
+
+  expect(outputNote.ownerHash).toBeDefined();
+  expect(outputNote.token).toBe("0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee");
+  expect(outputNote.amount).toBe("0");
+  expect(outputNote.viewTag.length).toBe(2);
+  expect(outputNote.viewTag).toBeDefined();
+  expect(outputNote.ephemeralKey).toBeDefined();
+});
+
+test("should pad output notes array", async () => {
+  const outputNote = generateDummyOutputNote();
+  const outputNotes = [outputNote];
+  const paddedOutputNotes = padAggregationOutputNotes(outputNotes);
+
+  expect(paddedOutputNotes.length).toBe(2);
+  expect(paddedOutputNotes[0]).toBe(outputNote);
+  expect(paddedOutputNotes[1]).toBeDefined();
+  expect(paddedOutputNotes[1].ownerHash).toBeDefined();
+  expect(paddedOutputNotes[1].token).toBe(
+    "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee"
+  );
+  expect(paddedOutputNotes[1].amount).toBe("0");
+  expect(paddedOutputNotes[1].viewTag.length).toBe(2);
+  expect(paddedOutputNotes[1].viewTag).toBeDefined();
+  expect(paddedOutputNotes[1].ephemeralKey).toBeDefined();
+});
+
+test("should generate output note hash", async () => {
+  const outputNote = generateDummyOutputNote();
+
+  const computedOutputNoteHash = poseidon3([
+    BigInt(outputNote.ownerHash),
+    BigInt(outputNote.amount),
+    BigInt(outputNote.token),
+  ]);
+
+  const outputNoteHash = generateOutputNoteHash(outputNote);
+  expect(outputNoteHash).toBe(computedOutputNoteHash);
+});
+
+test("should generate ephemeral keys hash", async () => {
+  const outputNotes = [generateDummyOutputNote(), generateDummyOutputNote()];
+
+  const computedEphemeralKeysHash = poseidon2(
+    outputNotes.map((note: any) => note.ephemeralKey)
+  );
+
+  const ephemeralKeysHash = generateEphemeralKeysHash(outputNotes);
+  expect(ephemeralKeysHash).toBe(computedEphemeralKeysHash);
+});
+
+test("should generate aggregation hash", async () => {
+    const outputNotes = [generateDummyOutputNote(), generateDummyOutputNote()];
+  
+    const computedOutputsHash = poseidon2(
+        outputNotes.map((note: any) => generateOutputNoteHash(note))
+      );
+
+    const computedEphemeralKeysHash = poseidon2(
+      outputNotes.map((note: any) => note.ephemeralKey)
+    );
+
+    const computedAggregationHash = poseidon2([computedOutputsHash, computedEphemeralKeysHash]);
+
+    const aggregationHash = generateAggregationHash(outputNotes);
+    expect(aggregationHash).toBe(computedAggregationHash);
+  });
+  
