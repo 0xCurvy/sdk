45d812a15d275e723b53d6f1dbd9881608ed0a0a
Lazar Travica <lazar.travica@route3.co>
2025-09-23 18:33:19 +0200

    wip: erc1155_onboarding estimation is almost working

--- src/types/meta-transaction.ts
+++ src/types/meta-transaction.ts
@@ -7,17 +7,18 @@ const META_TRANSACTION_TYPES = {
   ERC1155_ONBOARD: "erc1155_onboard",
 } as const;
 type META_TRANSACTION_TYPES = typeof META_TRANSACTION_TYPES;
-
 export { META_TRANSACTION_TYPES };
 export type MetaTransactionType = ExtractValues<META_TRANSACTION_TYPES>;
 
 const META_TRANSACTION_STATUSES = {
+  ESTIMATED: "estimated",
   PENDING: "pending",
   COMPLETED: "completed",
   FAILED: "failed",
   REJECTED: "rejected",
 } as const;
-
+type META_TRANSACTION_STATUSES = typeof META_TRANSACTION_STATUSES;
+export { META_TRANSACTION_STATUSES };
 export type MetaTransactionStatus = ExtractValues<typeof META_TRANSACTION_STATUSES>;
 
 export type MetaTransaction = {
@@ -25,13 +26,13 @@ export type MetaTransaction = {
   network: Network;
   currency: Currency;
   fromAddress: HexString;
+  toAddress?: HexString;
   type: MetaTransactionType;
-  payload: string;
+  gasFeeInCurrency: bigint;
+  curvyFeeInCurrency: bigint;
+  status?: MetaTransactionStatus;
   createdAt?: Date;
   updatedAt?: Date;
-  gasFee: bigint;
-  curvyFee: bigint;
-  status: MetaTransactionStatus;
 };
 
 export type MetaTransactionEstimate = CurvyCommandEstimate;
