28cfdcc638a7de00cf71fda31e278ebdc60bfd6b
Aleksandar Veljkovic <aleksandar.veljkovic.dev@gmail.com>
2025-10-08 12:10:44 +0200

    withdraw from collector's address

---
+++ src/planner/commands/erc1155/erc1155-explicit-withdraw-to-eoa.ts
@@ -0,0 +1,100 @@
+import { toBytes } from "viem";
+import { erc1155ABI } from "@/contracts/evm/abi";
+import type { ICurvySDK } from "@/interfaces/sdk";
+import type { CurvyCommandEstimate } from "@/planner/commands/abstract";
+import { AbstractErc1155Command } from "@/planner/commands/erc1155/abstract";
+import type { CurvyCommandData, CurvyIntent } from "@/planner/plan";
+import { type HexString, isHexString, META_TRANSACTION_TYPES } from "@/types";
+import { getMetaTransactionEip712HashAndSignedData } from "@/utils/meta-transaction";
+
+// This command automatically sends all available balance from CSUC to external address
+export class Erc1155ExplicitWithdrawToEOACommand extends AbstractErc1155Command {
+  #intent: CurvyIntent;
+
+  constructor(sdk: ICurvySDK, input: CurvyCommandData, intent: CurvyIntent) {
+    super(sdk, input);
+
+    if (!isHexString(intent.toAddress)) {
+      throw new Error("Erc1155ExplicitWithdrawToEOACommand: toAddress MUST be a hex string address");
+    }
+
+    if (!isHexString(intent.privateKey)) {
+        throw new Error("Erc1155ExplicitWithdrawToEOACommand: toAddress MUST be a hex string address");
+      }
+
+    this.#intent = intent;
+  }
+
+  async execute(): Promise<CurvyCommandData> {
+    const { id, gas, curvyFee } = await this.estimate();
+    const rpc = this.sdk.rpcClient.Network(this.input.networkSlug);
+
+    const privateKey = this.#intent.privateKey!;
+
+    const nonce = await rpc.provider.readContract({
+      abi: erc1155ABI,
+      address: this.network.erc1155ContractAddress as HexString,
+      functionName: "getNonce",
+      args: [this.input.source as HexString],
+    });
+
+    const tokenId = await rpc.provider.readContract({
+      abi: erc1155ABI,
+      address: this.network.erc1155ContractAddress as HexString,
+      functionName: "getTokenID",
+      args: [this.input.currencyAddress as HexString],
+    });
+
+    const amount = this.input.balance;
+
+    const totalFees = gas + curvyFee;
+
+    const effectiveAmount = amount - totalFees;
+
+    const [eip712Hash] = getMetaTransactionEip712HashAndSignedData(
+      this.input.source as HexString,
+      this.#intent.toAddress as HexString,
+      tokenId,
+      effectiveAmount,
+      totalFees,
+      nonce,
+      this.network.erc1155ContractAddress as HexString,
+      this.network.feeCollectorAddress as HexString,
+    );
+
+    const signature = (await this.sdk.rpcClient
+      .Network(this.input.networkSlug)
+      .signMessage(privateKey, { message: { raw: toBytes(eip712Hash) } })) as HexString;
+
+    await this.sdk.apiClient.metaTransaction.SubmitTransaction({ id, signature });
+
+    await this.sdk.pollForCriteria(
+      () => this.sdk.apiClient.metaTransaction.GetStatus(id),
+      (res) => {
+        if (res === "failed") throw new Error(`[ERC1155WithdrawToEoaCommand] Meta-transaction execution failed!`);
+        return res === "completed";
+      },
+    );
+
+    return {} as any;
+  }
+
+  async estimate(): Promise<CurvyCommandEstimate & { id: string }> {
+    const currencyAddress = this.input.currencyAddress;
+
+    const { id, gasFeeInCurrency, curvyFeeInCurrency } = await this.sdk.apiClient.metaTransaction.EstimateGas({
+      type: META_TRANSACTION_TYPES.ERC1155_WITHDRAW,
+      currencyAddress,
+      amount: this.input.balance.toString(),
+      fromAddress: this.input.source,
+      network: this.input.networkSlug,
+      toAddress: this.#intent.toAddress,
+    });
+
+    return {
+      gas: BigInt(gasFeeInCurrency ?? "0"),
+      curvyFee: BigInt(curvyFeeInCurrency ?? "0"),
+      id,
+    };
+  }
+}
--- src/planner/commands/index.ts
+++ src/planner/commands/index.ts
@@ -1,3 +1,4 @@
 export * from "./aggregator/aggregator-withdraw-to-erc1155";
 export * from "./erc1155/erc1155-withdraw-to-eoa";
+export * from "./erc1155/erc1155-explicit-withdraw-to-eoa";
 // TODO: Add other commands here
