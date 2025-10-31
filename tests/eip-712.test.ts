5feb444c10701b647005efa854a886ff39b8e8ad
Lazar Travica <lazar.travica@route3.co>
2025-10-31 14:48:08 +0100

    Fixed signature verification in Vault contract. Problem was whitespace in TYPE_HASH.

--- tests/eip-712.test.ts
+++ tests/eip-712.test.ts
@@ -1,12 +1,13 @@
 import { verifyTypedData } from "viem";
 import { privateKeyToAccount } from "viem/accounts";
 import { expect, test } from "vitest";
+import type { HexString } from "@/types";
 
 test("Unit test for signatures", async () => {
   const privateKey = "0xe6bd304017a184efa0f577139772305c1a7b64c0e5f7b0a9b6aa6a255469157d";
   const version = "1.0";
   const chainId = 31337;
-  const verifyingContract = "0x9a676e781a523b5d0c0e43731313a708cb607508";
+  const verifyingContract = "0x9a676e781a523b5d0c0e43731313a708cb607508" as HexString;
 
   const nonce = 0n;
   const from = "0x67fcb5316956053214374f37c53515aE3441b8EE";
@@ -18,8 +19,6 @@ test("Unit test for signatures", async () => {
 
   const account = privateKeyToAccount(privateKey);
 
-  // TODO: Zasto je Typescript jezik u kome kada prosledim ovo direktno kao argument literal lepo razume, ali kao promenljivu buni se?
-  // @ts-expect-error
   const _eip712Data = {
     domain: {
       name: "Curvy Privacy Vault",
@@ -27,7 +26,7 @@ test("Unit test for signatures", async () => {
       chainId,
       verifyingContract,
     },
-    primaryType: "CurvyMetaTransactionType",
+    primaryType: "CurvyMetaTransactionType" as const,
     types: {
       CurvyMetaTransactionType: [
         { name: "nonce", type: "uint256" },
@@ -50,35 +49,7 @@ test("Unit test for signatures", async () => {
     },
   };
 
-  const signature = await account.signTypedData({
-    domain: {
-      name: "Curvy Privacy Vault",
-      version,
-      chainId,
-      verifyingContract,
-    },
-    primaryType: "CurvyMetaTransactionType",
-    types: {
-      CurvyMetaTransactionType: [
-        { name: "nonce", type: "uint256" },
-        { name: "from", type: "address" },
-        { name: "to", type: "address" },
-        { name: "tokenId", type: "uint256" },
-        { name: "amount", type: "uint256" },
-        { name: "gasFee", type: "uint256" },
-        { name: "metaTransactionType", type: "uint8" },
-      ],
-    },
-    message: {
-      nonce,
-      from,
-      to,
-      tokenId,
-      amount,
-      gasFee,
-      metaTransactionType,
-    },
-  });
+  const signature = await account.signTypedData(_eip712Data);
 
   const goodSignature = await verifyTypedData({
     address: from,
