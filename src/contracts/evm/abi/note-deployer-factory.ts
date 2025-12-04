2b4b11bf37142b3d252f4153671bcd970e2207d1
radojevicMihailo <radojevic.mihailo.00@gmail.com>
2025-12-04 13:15:18 +0100

    build pass with curvy PORTALS

--- src/contracts/evm/abi/note-deployer-factory.ts
+++ src/contracts/evm/abi/note-deployer-factory.ts
@@ -1,99 +1,139 @@
 export const noteDeployerFactoryAbi = [
     {
-        inputs: [
+        "inputs": [
             {
-                internalType: "address",
-                name: "curvyAggregatorAlphaProxyAddress",
-                type: "address",
+                "internalType": "address",
+                "name": "curvyAggregatorAlphaProxyAddress",
+                "type": "address"
             },
             {
-                internalType: "address",
-                name: "curvyVaultProxyAddress",
-                type: "address",
+                "internalType": "address",
+                "name": "curvyVaultProxyAddress",
+                "type": "address"
             },
+            {
+                "internalType": "address",
+                "name": "lifiDiamondAddress",
+                "type": "address"
+            }
         ],
-        stateMutability: "nonpayable",
-        type: "constructor",
+        "stateMutability": "nonpayable",
+        "type": "constructor"
     },
     {
-        inputs: [
+        "inputs": [
             {
-                components: [
+                "internalType": "bytes",
+                "name": "_bridgeData",
+                "type": "bytes"
+            },
+            {
+                "components": [
                     {
-                        internalType: "uint256",
-                        name: "ownerHash",
-                        type: "uint256",
+                        "internalType": "uint256",
+                        "name": "ownerHash",
+                        "type": "uint256"
                     },
                     {
-                        internalType: "uint256",
-                        name: "token",
-                        type: "uint256",
+                        "internalType": "uint256",
+                        "name": "token",
+                        "type": "uint256"
                     },
                     {
-                        internalType: "uint256",
-                        name: "amount",
-                        type: "uint256",
-                    },
+                        "internalType": "uint256",
+                        "name": "amount",
+                        "type": "uint256"
+                    }
                 ],
-                internalType: "struct CurvyTypes.Note",
-                name: "note",
-                type: "tuple",
-            },
+                "internalType": "struct CurvyTypes.Note",
+                "name": "note",
+                "type": "tuple"
+            }
         ],
-        name: "deploy",
-        outputs: [],
-        stateMutability: "payable",
-        type: "function",
+        "name": "bridgeAndDeploy",
+        "outputs": [],
+        "stateMutability": "payable",
+        "type": "function"
     },
     {
-        inputs: [
+        "inputs": [
             {
-                internalType: "uint256",
-                name: "ownerHash",
-                type: "uint256",
-            },
+                "components": [
+                    {
+                        "internalType": "uint256",
+                        "name": "ownerHash",
+                        "type": "uint256"
+                    },
+                    {
+                        "internalType": "uint256",
+                        "name": "token",
+                        "type": "uint256"
+                    },
+                    {
+                        "internalType": "uint256",
+                        "name": "amount",
+                        "type": "uint256"
+                    }
+                ],
+                "internalType": "struct CurvyTypes.Note",
+                "name": "note",
+                "type": "tuple"
+            }
         ],
-        name: "getContractAddress",
-        outputs: [
-            {
-                internalType: "address",
-                name: "",
-                type: "address",
-            },
-        ],
-        stateMutability: "view",
-        type: "function",
+        "name": "deploy",
+        "outputs": [],
+        "stateMutability": "payable",
+        "type": "function"
     },
     {
-        inputs: [
+        "inputs": [
             {
-                internalType: "uint256",
-                name: "ownerHash",
-                type: "uint256",
-            },
+                "internalType": "uint256",
+                "name": "ownerHash",
+                "type": "uint256"
+            }
         ],
-        name: "getCreationCode",
-        outputs: [
+        "name": "getContractAddress",
+        "outputs": [
             {
-                internalType: "bytes",
-                name: "",
-                type: "bytes",
-            },
+                "internalType": "address",
+                "name": "",
+                "type": "address"
+            }
         ],
-        stateMutability: "pure",
-        type: "function",
+        "stateMutability": "view",
+        "type": "function"
     },
     {
-        inputs: [],
-        name: "noteDeployer",
-        outputs: [
+        "inputs": [
             {
-                internalType: "contract INoteDeployer",
-                name: "",
-                type: "address",
-            },
+                "internalType": "uint256",
+                "name": "ownerHash",
+                "type": "uint256"
+            }
         ],
-        stateMutability: "view",
-        type: "function",
+        "name": "getCreationCode",
+        "outputs": [
+            {
+                "internalType": "bytes",
+                "name": "",
+                "type": "bytes"
+            }
+        ],
+        "stateMutability": "pure",
+        "type": "function"
     },
+    {
+        "inputs": [],
+        "name": "noteDeployer",
+        "outputs": [
+            {
+                "internalType": "contract INoteDeployer",
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    }
 ] as const;
