69994c99d080b2744962864b8f42e42380398b2f
Vanja Obradovic <vanja.obradovic99@gmail.com>
2026-02-10 10:30:18 +0100

    fix: exit bridge status polling

--- src/contracts/evm/abi/index.ts
+++ src/contracts/evm/abi/index.ts
@@ -1,5 +1,6 @@
 export { aggregatorAlphaAbi } from "./aggregator-alpha";
 export { legacyErc20PortalAbi } from "./legacy-erc20-portal";
+export { lifiDiamondFragmentAbi } from "./lifiDiamondFragment";
 export { evmMulticall3Abi } from "./multicall3";
 export { portalFactoryAbi } from "./portal-factory";
 export { tokenBridgeAbi } from "./token-bridge";
